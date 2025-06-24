848396ecdd59eed0f0adec3d49b9b641fa865821
KlausFolzBrunner <klausjurgenfolz@gmail.com>
2025-06-24 14:13:26 +0200

    Fixed the comments baderna

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from src.PyM.MExpression import MExpression
 import os
 
 def main():
-    file_path = os.path.join('queries', 'simple_query.txt')
+    file_path = os.path.join('queries', 'table_nested_join.txt')
     
     with open(file_path, 'r', encoding='utf-8') as file:
         query = file.read()
 
+   
     expr = MExpression(query)
-    expr.write_tokens_to_txt("tokens.txt")
-
+    no_comments = expr.remove_comments()
+    #print("M Expression without comments:")
+    #print(no_comments)
+    print(expr.extract_comments())
     
 if __name__ == '__main__':
     main()
--- src/PyM/MExpression.py
+++ src/PyM/MExpression.py
@@ -7,8 +7,11 @@ class MExpression:
         self.m_expression: str = m_expression
         self.input_stream: InputStream = InputStream(m_expression)
         self.lexer: PowerQueryLexer = PowerQueryLexer(self.input_stream)
-        self.lexer.removeErrorListeners()
-        
+        #self.lexer.removeErrorListeners()
+    
+    def __str__(self) -> str:
+        """Returns the M expression as a string"""
+        return self.m_expression
         
     def remove_comments(self) -> str:
         """Removes comments from the M expression
@@ -21,17 +24,42 @@ class MExpression:
         result: list = []
 
         while token.type != Token.EOF:
-            if token.channel != PowerQueryLexer.COMMENT:
+            if token.channel != PowerQueryLexer.COMMENTCHANNEL:
                 result.append(token.text)
+            
             token = self.lexer.nextToken()
         
         return ''.join(result)
     
-    def print_tokens(self) -> None:
+    def extract_comments(self) -> list[str]:
+        """Extracts comments from the DAX expression
+
+        Returns:
+            list[str]: List of comments in the DAX expression
+        """
+        self.lexer.reset()  # Reset the lexer to start from the beginning
+        comments: list[str] = []
+        token: Token = self.lexer.nextToken()
+        
+        while token.type != Token.EOF:
+            if token.channel == PowerQueryLexer.COMMENTCHANNEL:
+                comments.append(token.text)
+            
+            token = self.lexer.nextToken()
+        
+        return comments
+        
+    
+    def print_tokens(self, ignore_irrelevant_chars: bool = True) -> None: 
         """Prints the tokens of the M expression"""
         self.lexer.reset()
         token: Token = self.lexer.nextToken()
         while token.type != Token.EOF:
+            
+            if ignore_irrelevant_chars and token.channel == PowerQueryLexer.IRRELEVANTCHARS:
+                token = self.lexer.nextToken()
+                continue
+            
             print(f"Token: {token.text}, Type: {self.lexer.symbolicNames[token.type]}, Number: {token.type}")
             token = self.lexer.nextToken()
     
